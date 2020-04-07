d6d657496abe46970734b41e904b67805886658d
Imran <62325347+itsimranmd@users.noreply.github.com>
2020-04-07 13:04:12 +0530

    Added MockData
    Added mock data and connected the home page to the route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./styles.css";
 import { Button } from "reactstrap";
 import Header from "./Components/Header";
+import { Route, Switch } from "react-router-dom";
+import routes from "./routes/routes";
+import Home from "./Pages/Home";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route path={routes.home} />
+        <Home />
+      </Switch>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,10 +7,6 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   NavbarText
 } from "reactstrap";
 
@@ -34,17 +30,6 @@ const Header = ({}) => {
                 Authors
               </NavLink>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
           </Nav>
           <NavbarText>Simple Text</NavbarText>
         </Collapse>
